a05bdf13d35870eada93dfe7ebcb42078a6567b4
tango <veigar@dokov.com>
2023-12-16 21:46:44 +0100

    database and admin page

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        """ Change name frmo djanbo admin from Categorys to Categories"""
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.ImageField(default=0)
+
+    def __str__(self):
+        return self.title
